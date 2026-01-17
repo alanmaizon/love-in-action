52efa5bedc97c0991c848e8e5ba0db748f1f8e97
Alan Maizon <alanmaizon@hotmail.com>
2026-01-17 20:40:59 +0000

    feat: Add Render Blueprint for infrastructure setup and deployment configurations

--- love_backend/build.sh
+++ love_backend/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
-set -o errexit
+# Render build script for Django
+set -o errexit  # Exit on error
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# Seed charities if needed (idempotent - won't duplicate)
+python manage.py seed_charities
